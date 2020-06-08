f12a325dacb1ea217dd739c229b1e824b4b87134
TheGetch <TheGetch@users.noreply.github.com>
2020-06-08 11:31:40 -0400

    Added seclist, impacket

--- kali_additions.sh
+++ kali_additions.sh
@@ -27,8 +27,21 @@ rm master.zip
 echo '########################### Installing KeePassXC'
 apt install keepassxc
 
+echo '########################### Installing GoLang'
+apt install golang
+
 echo '########################### Installing pip'
-apt install python-pip
+apt install python-pip && apt install python3-pip
 
 echo '########################### Installing neo4j'
 apt install neo4j
+
+echo '########################### Installing seclists'
+apt -y install seclists 
+
+echo '########################### Installing Impacket'
+cd /opt/
+wget https://github.com/SecureAuthCorp/impacket/releases/download/impacket_0_9_21/impacket-0.9.21.tar.gz
+tar -xvzf impacket-0.9.21.tar.gz
+python3 -m pip install impacket-0.9.21/
+rm impacket-0.9.21.tar.gz
